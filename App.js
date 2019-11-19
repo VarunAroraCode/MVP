5e6540540dc0ba25fc3bbf2ddd4de067559be862
tarora2014 <tarora@g.clemson.edu>
2019-11-18 23:27:31 -0800

    chart registering except for team stats

--- App.js
+++ App.js
@@ -77,21 +77,7 @@ export default class App extends Component {
       team: 'select your Team',
       tableHead: ["   ", 'Top', 'Jungler', 'Mid', 'Carry', 'Flex', 'Team'],
     };
-    // const playerTop = this.state.top;
-    // const playerJungler = this.state.jungler;
-    // const playerMid = this.state.mid;
-    // const playerCarry = this.state.carry;
-    // const playerFlex = this.state.flex;
-    // const playerTeam = this.state.team;
-    // const topData = dummyData[0].playerTop;
-    // const junglerData = dummyData[3].playerJungler;
-    // const midData = dummyData[1].playerMid;
-    // const carryData = dummyData[2].playerCarry;
-    // const flexData = dummyData[4].playerFlex;
-    // const teamData = dummyData[5].playerTeam;
   }
-
-
   handleRecruitClick(){
     this.setState({selectionRender : false})
   };
@@ -106,14 +92,117 @@ export default class App extends Component {
     const playerCarry = this.state.carry;
     const playerFlex = this.state.flex;
     const playerTeam = this.state.team;
-    const topData = dummyData[0].playerTop;
-    const midData = dummyData[1].playerMid;
-    const carryData = dummyData[2].playerCarry;
-    const junglerData = dummyData[3].playerJungler;
-    const flexData = dummyData[4].playerFlex;
-    const teamData = dummyData[5].playerTeam;
-    const KiinArray = dummyData[0].Kiin;
- 
+    //-----------------------------------------
+    const topData = [];
+    if (playerTop === dummyData[0].top[0].Smeb[0]){
+      for(var i = 0; i < 5; i++)
+        topData.push(dummyData[0].top[0].Smeb[i]);
+    }
+    else if (playerTop === dummyData[0].top[1].Kiin[0]){
+      for(var i = 0; i < 5; i++)
+        topData.push(dummyData[0].top[1].Kiin[i]);
+    }
+    else if (playerTop === dummyData[0].top[2].LetMe[0]){
+      for(var i = 0; i < 5; i++)
+        topData.push(dummyData[0].top[2].LetMe[i]);
+    }
+    else if (playerTop === dummyData[0].top[3].CuVee[0]){
+      for(var i = 0; i < 5; i++)
+        topData.push(dummyData[0].top[3].CuVee[i]);
+    }
+    else if (playerTop === dummyData[0].top[4].Ssumday[0]){
+      for(var i = 0; i < 5; i++)
+        topData.push(dummyData[0].top[4].Ssumday[i]);
+    }
+    //---------------------------------------------------
+    const midData = [];
+    if (playerMid === dummyData[1].mid[0].Maple[0]){
+      for(var i = 0; i < 5; i++)
+        midData.push(dummyData[1].mid[0].Maple[i]);
+    }
+    else if (playerMid === dummyData[1].mid[1].Caps[0]){
+      for(var i = 0; i < 5; i++)
+        midData.push(dummyData[1].mid[1].Caps[i]);
+    }
+    else if (playerMid === dummyData[1].mid[2].Kuro[0]){
+      for(var i = 0; i < 5; i++)
+        midData.push(dummyData[1].mid[2].Kuro[i]);
+    }
+    else if (playerMid === dummyData[1].mid[3].Yukal[0]){
+      for(var i = 0; i < 5; i++)
+        midData.push(dummyData[1].mid[3].Yukal[i]);
+    }
+    else if (playerMid === dummyData[1].mid[4].Rookie[0]){
+      for(var i = 0; i < 5; i++)
+        midData.push(dummyData[1].mid[4].Rookie[i]);
+    }
+    //------------------------------------------------------
+    const JunData = [];
+    if (playerJungler === dummyData[3].jungler[0].Karsa[0]){
+      for(var i = 0; i < 5; i++)
+        JunData.push(dummyData[3].jungler[0].Karsa[i]);
+    }
+    else if (playerJungler === dummyData[3].jungler[1].DNK[0]){
+      for(var i = 0; i < 5; i++)
+        JunData.push(dummyData[3].jungler[1].DNK[i]);
+    }
+    else if (playerJungler === dummyData[3].jungler[2].Xerxe[0]){
+      for(var i = 0; i < 5; i++)
+        JunData.push(dummyData[3].jungler[2].Xerxe[i]);
+    }
+    else if (playerJungler === dummyData[3].jungler[3].Shrimp[0]){
+      for(var i = 0; i < 5; i++)
+        JunData.push(dummyData[3].jungler[3].Shrimp[i]);
+    }
+    else if (playerJungler === dummyData[3].jungler[4].Canyon[0]){
+      for(var i = 0; i < 5; i++)
+        JunData.push(dummyData[3].jungler[4].Canyon[i]);
+    }
+  //---------------------------------------------------------
+  const carryData = [];
+  if (playerCarry === dummyData[2].carry[0].Uzi[0]){
+    for(var i = 0; i < 5; i++)
+     carryData.push(dummyData[2].carry[0].Uzi[i]);
+  }
+  else if (playerCarry === dummyData[2].carry[1].Deft[0]){
+    for(var i = 0; i < 5; i++)
+     carryData.push(dummyData[2].carry[1].Deft[i]);
+  }
+  else if (playerCarry === dummyData[2].carry[2].Ruler[0]){
+    for(var i = 0; i < 5; i++)
+     carryData.push(dummyData[2].carry[2].Ruler[i]);
+  }
+  else if (playerCarry === dummyData[2].carry[3].Jackeylove[0]){
+    for(var i = 0; i < 5; i++)
+     carryData.push(dummyData[2].carry[3].Jackeylove[i]);
+  }
+  else if (playerCarry === dummyData[2].carry[4].Doublelift[0]){
+    for(var i = 0; i < 5; i++)
+     carryData.push(dummyData[2].carry[4].Doublelift[i]);
+  }
+    //---------------------------------------------------------
+    const flexData = [];
+    if (playerFlex === dummyData[4].flex[1].Kiin[0]){
+      for(var i = 0; i < 5; i++)
+        flexData.push(dummyData[4].flex[1].Kiin[i]);
+    }
+    else if (playerFlex === dummyData[4].flex[6].Deft[0]){
+      for(var i = 0; i < 5; i++)
+        flexData.push(dummyData[4].flex[6].Deft[i]);
+    }
+    else if (playerFlex === dummyData[4].flex[11].Caps[0]){
+      for(var i = 0; i < 5; i++)
+        flexData.push(dummyData[4].flex[11].Caps[i]);
+    }
+    else if (playerFlex === dummyData[4].flex[8].Jackeylove[0]){
+      for(var i = 0; i < 5; i++)
+        flexData.push(dummyData[4].flex[8].Jackeylove[i]);
+    }
+    else if (playerFlex === dummyData[4].flex[4].Ssumday[0]){
+      for(var i = 0; i < 5; i++)
+        flexData.push(dummyData[4].flex[4].Ssumday[i]);
+    }
+    
     //----------------------------------------------------------
     // Renders the first page, where the individual gets tho choose their players for their Roster
     //----------------------------------------------------------
@@ -265,12 +354,12 @@ export default class App extends Component {
             <Row data={this.state.tableHead} style={styles.head} textStyle={styles.text}/>
             <Rows data={
               [
-                ["Player", playerTop, playerJungler, playerMid, playerCarry, playerFlex, playerTeam],
-                ["Kills",  KiinArray[1], KiinArray[2], KiinArray[3], KiinArray[4],KiinArray[5]],
-                ["Deaths", this.state.top, this.state.jungler,this.state.mid,this.state.carry,this.state.flex,this.state.team],
-                ["Assists", this.state.top, this.state.jungler,this.state.mid,this.state.carry,this.state.flex,this.state.team],
-                ["CS", this.state.top, this.state.jungler,this.state.mid,this.state.carry,this.state.flex,this.state.team]
-              ]
+                ["Player" , topData[0], JunData[0], midData[0], carryData[0], flexData[0], carryData[0]],
+                ["Kills" ,  topData[1], JunData[1], midData[1], carryData[1], flexData[1], carryData[1]],
+                ["Deaths" , topData[2], JunData[2], midData[2], carryData[2], flexData[2], carryData[2]],
+                ["Assists", topData[3], JunData[3], midData[3], carryData[3], flexData[3], carryData[3]],
+                ["CS"     , topData[4], JunData[4], midData[4], carryData[4], flexData[4], carryData[4]]
+              ] 
             } textStyle={styles.text}/>
           </Table>
       </View>
@@ -285,10 +374,10 @@ export default class App extends Component {
             <Rows data={
               [
                 ["Player", "Faker", "Incarnation","Jensen","Reckless","Impact","100 Theives"],
-                ["Kills", 6, 4,7,9,7, 22],
+                ["Kills", 6, 4,7,9,7, 34],
                 ["Deaths", 7, 4,9,2,8, 16],
-                ["Assists", 7, 8,7,9,7, 19],
-                ["CS", 12, 10, 19,14,12,9, 17]
+                ["Assists", 7, 8,7,9,7, 34],
+                ["CS", 12, 10, 19,14,12,9, 88]
               ]
               
             } textStyle={styles.text}/>
--- dummyData.js
+++ dummyData.js
@@ -1,4 +1,3 @@
-
 var dummyData = [
     {top : [
         {Smeb: ['Smeb',5, 2, 7, 4]},
@@ -8,25 +7,25 @@ var dummyData = [
         {Ssumday: ["Ssumday", 3, 3, 8, 3]}
     ]},
     {mid : [
-        {Maple: ['Maple',5, 2, 7, 4]},
-        {Caps: ["Caps",3, 4, 12, 2]},
-        {Kuro: ["Kuro", 2, 3, 16, 2]},
-        {Yukal: ["Yukal", 6, 3, 3, 9]},
-        {Rookie: ["Rookie", 3, 3, 8, 3]}
+        {Maple: ['Maple',6, 5, 12, 19]},
+        {Caps: ["Caps",7, 8, 12, 15]},
+        {Kuro: ["Kuro", 1, 5, 19, 25]},
+        {Yukal: ["Yukal", 11, 3, 9, 20]},
+        {Rookie: ["Rookie", 4, 1, 18, 13]}
     ]},
     {carry : [
-        {Uzi: ['Uzi',5, 2, 7, 4]},
-        {Deft: ["Deft",3, 4, 12, 2]},
-        {Ruler: ["Ruler", 2, 3, 16, 2]},
-        {Jackeylove: ["Jackeylove", 6, 3, 3, 9]},
-        {Doublelift: ["Doublelift", 3, 3, 8, 3]}
+        {Uzi: ['Uzi',6, 3, 12, 14]},
+        {Deft: ["Deft",13, 4, 5, 12]},
+        {Ruler: ["Ruler", 5, 7, 20, 15]},
+        {Jackeylove: ["Jackeylove", 17, 5, 3, 12]},
+        {Doublelift: ["Doublelift", 16, 4, 1, 10]}
     ]},
     {jungler : [
-        {Karsa: ['Karsa',5, 2, 7, 4]},
-        {DNK: ["DNK",3, 4, 12, 2]},
-        {Xerxe: ["Xerxe", 2, 3, 16, 2]},
-        {Shrimp: ["Shrimp", 6, 3, 3, 9]},
-        {Canyon: ["Canyon", 3, 3, 8, 3]}
+        {Karsa: ['Karsa',7, 9, 7, 24]},
+        {DNK: ["DNK",6, 7, 17, 12]},
+        {Xerxe: ["Xerxe", 4, 10, 11, 22]},
+        {Shrimp: ["Shrimp", 5, 7, 13, 19]},
+        {Canyon: ["Canyon", 8, 8, 9, 23]}
     ]},
     {flex : [
         {Smeb: ['Smeb',5, 2, 7, 4]},
@@ -34,21 +33,21 @@ var dummyData = [
         {LetMe: ["LetMe", 2, 3, 16, 2]},
         {CuVee: ["CuVee", 6, 3, 3, 9]},
         {Ssumday: ["Ssumday", 3, 3, 8, 3]},
-        {Uzi: ['Uzi',5, 2, 7, 4]},
-        {Deft: ["Deft",3, 4, 12, 2]},
-        {Ruler: ["Ruler", 2, 3, 16, 2]},
-        {Jackeylove: ["Jackeylove", 6, 3, 3, 9]},
-        {Doublelift: ["Doublelift", 3, 3, 8, 3]},
-        {Maple: ['Maple',5, 2, 7, 4]},
-        {Caps: ["Caps",3, 4, 12, 2]},
-        {Kuro: ["Kuro", 2, 3, 16, 2]},
-        {Yukal: ["Yukal", 6, 3, 3, 9]},
-        {Rookie: ["Rookie", 3, 3, 8, 3]},
-        {Karsa: ['Karsa',5, 2, 7, 4]},
-        {DNK: ["DNK",3, 4, 12, 2]},
-        {Xerxe: ["Xerxe", 2, 3, 16, 2]},
-        {Shrimp: ["Shrimp", 6, 3, 3, 9]},
-        {Canyon: ["Canyon", 3, 3, 8, 3]}
+        {Uzi: ['Uzi',6, 3, 12, 14]},
+        {Deft: ["Deft",13, 4, 5, 12]},
+        {Ruler: ["Ruler", 5, 7, 20, 15]},
+        {Jackeylove: ["Jackeylove", 17, 5, 3, 12]},
+        {Doublelift: ["Doublelift", 16, 4, 1, 10]},
+        {Maple: ['Maple',6, 5, 12, 19]},
+        {Caps: ["Caps",7, 8, 12, 15]},
+        {Kuro: ["Kuro", 1, 5, 19, 25]},
+        {Yukal: ["Yukal", 11, 3, 9, 20]},
+        {Rookie: ["Rookie", 4, 1, 18, 13]},
+        {Karsa: ['Karsa',7, 9, 7, 24]},
+        {DNK: ["DNK",6, 7, 17, 12]},
+        {Xerxe: ["Xerxe", 4, 10, 11, 22]},
+        {Shrimp: ["Shrimp", 5, 7, 13, 19]},
+        {Canyon: ["Canyon", 8, 8, 9, 23]}
     ]},
     {team : [
         {SKTT1: 'SKT T1'},
@@ -56,6 +55,11 @@ var dummyData = [
         {G2Esports: 'G2 Esports'},
         {Cloud9:'Cloud9'},
         {TeamLiquid:'Team Liquid'}
+        // {SKTT1: ['SKT T1', 44, 15, 44, 93]},
+        // {SamsungWhite:['Samsung White', 39, 18, 39, 88]},
+        // {G2Esports: ['G2 Esports', 47, 19, 47, 99]},
+        // {Cloud9:['Cloud9', 40, 13, 40, 94]},
+        // {TeamLiquid:['Team Liquid', 45, 14, 45, 90]}
     ]}
 
 ]
